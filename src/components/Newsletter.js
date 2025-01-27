0c172e800c21796883d6c7d6cad315ddf2d6982f
Mukul <119119230+Mukul7Raj@users.noreply.github.com>
2025-01-27 11:12:16 +0530

    Update Newsletter.js

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Col, Row, Alert } from "react-bootstrap";
+import {Alert } from "react-bootstrap";
 
 export const Newsletter = ({ status, message, onValidated }) => {
   const [email, setEmail] = useState('');
